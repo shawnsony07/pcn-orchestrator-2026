5c012753cef9260acd1c1843bed06dbd69702630
Shawn Sony <106821199+shawnsony07@users.noreply.github.com>
2026-08-26 10:16:46 +0530

    chore(hal): PCN-driven create hal/hal_bme280.h

---
+++ hal/hal_bme280.h
@@ -0,0 +1,15 @@
+#ifndef HAL_BME280_H
+#define HAL_BME280_H
+
+/*
+ * HAL Driver for BME280 Temperature, Pressure, and Humidity Sensor.
+ * Note: BME280 is obsolete and replaced by BME688.
+ * Updating configuration to use BME688.
+ */
+
+#define BME688_I2C_ADDRESS_PRIMARY   0x76
+#define BME688_I2C_ADDRESS_SECONDARY 0x77
+
+#define BME688_CHIP_ID               0x61
+
+#endif // HAL_BME280_H
