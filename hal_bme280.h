591d4a1716ccf43048c47aaa02ebd3907b180d9d
Shawn Sony <106821199+shawnsony07@users.noreply.github.com>
2026-08-30 23:41:15 +0530

    chore(hal): PCN-driven create hal_bme280.h

---
+++ hal_bme280.h
@@ -0,0 +1,11 @@
+#ifndef HAL_BME280_H
+#define HAL_BME280_H
+
+/*
+ * HAL Header for BME280
+ * Automatically updated to reference replacement part BME688
+ */
+
+#define BME280_REPLACEMENT_PART_NUMBER "BME688"
+
+#endif /* HAL_BME280_H */
